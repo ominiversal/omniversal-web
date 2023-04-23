39e5f97b7d0fd338899511fa0177f3bb6adead75
Omniversal <omniversal@Monicas-MacBook-Pro.local>
2023-04-23 03:53:08 -0500

    completed hero

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
+import React from 'react';
+import styled from 'styled-components';
+
 import './App.css';
+import Hero from './components/Hero'
+
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    <Main>
+      <Hero />
+    </Main>
   );
 }
 
 export default App;
+
+const Main = styled.div`
+  color: black;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  font-size: 36px;
+  font-weight: bold;
+  padding-top: 5px;
+  font-family: 'Montserrat', sans-serif;
+  height: 100vh;
+`;
